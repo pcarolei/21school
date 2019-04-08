15469d05ca97abbb67b69569cda4a793af86a02a
pcarolei <peppermintcarolei@gmail.com>
2019-04-09 02:30:47 +0300

    +ft_putendl_fd

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -1,21 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/09 02:20:27 by pcarolei          #+#    #+#             */
-/*   Updated: 2019/04/09 02:29:49 by pcarolei         ###   ########.fr       */
+/*   Created: 2019/04/09 02:27:55 by pcarolei          #+#    #+#             */
+/*   Updated: 2019/04/09 02:30:22 by pcarolei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef LIBFT_H
-#define LIBFT_H
-#include <unistd.h>
-void	ft_putchar(char c);
-void	ft_putstr(const char *s);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char const *s, int fd);
-void	ft_putendl_fd(char const *s, int fd);
 
-#endif
+#include "libft.h"
+
+void	ft_putendl_fd(char const *s, int fd)
+{
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
+}
