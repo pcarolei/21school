e2166d5553f950f20922826224915e13c176448e
pcarolei <peppermintcarolei@gmail.com>
2019-04-09 05:53:42 +0300

    added some macro

--- libft/libft.h
+++ libft/libft.h
@@ -6,12 +6,18 @@
 /*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 02:20:27 by pcarolei          #+#    #+#             */
-/*   Updated: 2019/04/09 02:33:21 by pcarolei         ###   ########.fr       */
+/*   Updated: 2019/04/09 04:53:25 by pcarolei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #ifndef LIBFT_H
-#define LIBFT_H
-#include <unistd.h>
+# define LIBFT_H
+# define printi(s, n) printf("%s %d\n", s, n)
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+char	*ft_itoa(int n);
 void	ft_putchar(char c);
 void	ft_putstr(const char *s);
 void	ft_putendl(const char *s);
