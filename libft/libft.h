c70a886c4c692452b042e6e2efa7a2e2290ab0ed
pcarolei <peppermintcarolei@gmail.com>
2019-04-11 00:14:37 +0300

    +ft_toupper

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 02:20:27 by pcarolei          #+#    #+#             */
-/*   Updated: 2019/04/11 00:07:27 by pcarolei         ###   ########.fr       */
+/*   Updated: 2019/04/11 00:09:25 by pcarolei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_toupper(int c);
 
 char	*ft_itoa(int n);
 void	ft_putchar(char c);
