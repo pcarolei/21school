a151059908f367b1149fdf684b62ac2e99e0cb30
pcarolei <peppermintcarolei@gmail.com>
2019-04-10 23:45:44 +0300

    +ft_isalpha

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -1,30 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/09 02:20:27 by pcarolei          #+#    #+#             */
-/*   Updated: 2019/04/10 23:42:37 by pcarolei         ###   ########.fr       */
+/*   Created: 2019/04/10 23:44:11 by pcarolei          #+#    #+#             */
+/*   Updated: 2019/04/10 23:45:13 by pcarolei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <unistd.h>
-# include <stdlib.h>
-# include <string.h>
-
-int		ft_isalpha(int c);
-
-char	*ft_itoa(int n);
-void	ft_putchar(char c);
-void	ft_putstr(const char *s);
-void	ft_putendl(const char *s);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char const *s, int fd);
-void	ft_putendl_fd(char const *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-
-#endif
+int	ft_isalpha(int c)
+{
+	return ((c >= 65 && c <= 90)
+			|| (c >= 97 && c <= 122));
+}
