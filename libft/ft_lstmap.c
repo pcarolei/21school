b3ee6dc1da09002e790d43f85c3e181f333114b6
pcarolei <peppermintcarolei@gmail.com>
2019-04-17 19:28:43 +0300

    fixes

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -5,17 +5,18 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/16 05:45:54 by pcarolei          #+#    #+#             */
-/*   Updated: 2019/04/17 17:24:20 by pcarolei         ###   ########.fr       */
+/*   Created: 2019/04/17 18:46:32 by pcarolei          #+#    #+#             */
+/*   Updated: 2019/04/17 19:27:42 by pcarolei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+t_list		*ft_lstmap(t_list *lst, t_list *(f)(t_list *elem))
 {
 	t_list	*ptr;
 	t_list	*newlst;
+	t_list	*proxy;
 
 	if (!lst || !f)
 		return (NULL);
@@ -23,7 +24,10 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	newlst = NULL;
 	while (ptr)
 	{
-		ft_lstaddl(&newlst, f(ptr));
+		proxy = ft_lstnew(ptr->content, ptr->content_size);
+		if (!proxy)
+			return (newlst);
+		ft_lstaddl(&newlst, f(proxy));
 		ptr = ptr->next;
 	}
 	return (newlst);
