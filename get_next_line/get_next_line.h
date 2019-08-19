d7efd3a406b22a5141f0eafab1e93ea3a13d77a7
pcarolei <peppermintcarolei@gmail.com>
2019-08-19 14:55:44 +0300

    get_next_line done. 125/100

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/19 11:06:03 by pcarolei          #+#    #+#             */
+/*   Updated: 2019/08/19 12:28:29 by pcarolei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <stdlib.h>
+# include "./libft/libft.h"
+
+typedef struct	s_fd_list
+{
+	int					fd;
+	char				*fd_str;
+	char				*tmp;
+	struct s_fd_list	*next;
+}				t_fd_list;
+
+int				get_next_line(const int fd, char **line);
+# define NODE (find_node_by_fd(fd, fd_list))
+
+#endif
---
+++ get_next_line/libft/ft_strstr.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/12 02:45:41 by pcarolei          #+#    #+#             */
+/*   Updated: 2019/04/12 02:47:03 by pcarolei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	return (ft_strnstr(haystack, needle, ft_strlen(haystack) + 1));
+}
