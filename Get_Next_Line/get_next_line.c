3b8d52bb98fa40207444cff5e7253227d44c6df0
pcarolei <peppermintcarolei@gmail.com>
2019-08-19 11:53:59 +0300

    get_next_line almost done

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -5,60 +5,88 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/29 06:10:17 by pcarolei          #+#    #+#             */
-/*   Updated: 2019/08/05 02:58:33 by pcarolei         ###   ########.fr       */
+/*   Created: 2019/08/19 11:02:01 by pcarolei          #+#    #+#             */
+/*   Updated: 2019/08/19 11:12:49 by pcarolei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*split_by_nl(char *fd_str, int opt)
+t_fd_list	*create_node(int fd)
 {
-	size_t	n_pos;
-	char	*new_line;
+	t_fd_list	*new;
 
-	if (!ft_strchr(fd_str, '\n'))
-		return ((opt == 1) ? (fd_str) : (NULL));
-	n_pos = ft_strchr(fd_str, '\n') - fd_str;
-	if (opt == 1)
-		return (ft_strsub(fd_str, 0, n_pos));
-	new_line = ft_strsub(fd_str, n_pos + 1, ft_strlen(fd_str));
-	free(fd_str);
-	return (new_line);
+	new = (t_fd_list *)malloc(sizeof(t_fd_list));
+	if (!new)
+		return (NULL);
+	new->fd = fd;
+	new->fd_str = ft_strnew(0);
+	if (!new->fd_str)
+		return (NULL);
+	new->tmp = NULL;
+	new->next = NULL;
+	return (new);
 }
 
-char	*concat_strings(char *fd_str, char *read_buf)
+t_fd_list	*find_node_by_fd(const int needed_fd, t_fd_list *fd_list)
 {
-	char	*new_fd_str;
+	t_fd_list	*ptr;
 
-	new_fd_str = ft_strjoin(fd_str, read_buf);
-	free(fd_str);
-	return (new_fd_str);
+	ptr = fd_list;
+	if (!fd_list || (ptr->fd == needed_fd))
+		return (ptr);
+	while (ptr->next != NULL)
+	{
+		if (ptr->fd == needed_fd)
+			return (ptr);
+		ptr = ptr->next;
+	}
+	ptr->next = create_node(needed_fd);
+	return (ptr->next);
 }
 
-int		get_next_line(const int fd, char **line)
+int			split_str_by_newline(t_fd_list *cur_node, char **line)
 {
-	int			ret;
-	char		read_buf[BUFF_SIZE + 1];
-	static char	*fd_str;
+	char	*pos;
 
-	if (fd < 0 || BUFF_SIZE < 0 || !line)
+	if (ft_strlen(cur_node->fd_str) == 0)
+		return (0);
+	pos = ft_strchr(cur_node->fd_str, '\n');
+	if (pos == NULL)
+	{
+		*line = ft_strdup(cur_node->fd_str);
+		free(cur_node->fd_str);
+		cur_node->fd_str = ft_strnew(0);
+		return (((*line) == NULL) ? (-1) : (1));
+	}
+	pos[0] = '\0';
+	*line = ft_strdup(cur_node->fd_str);
+	cur_node->tmp = cur_node->fd_str;
+	cur_node->fd_str = ft_strdup(&(pos[1]));
+	free(cur_node->tmp);
+	return (((*line) == NULL || cur_node->fd_str == NULL) ? (-1) : (1));
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	int					ret;
+	char				read_buf[BUFF_SIZE + 1];
+	static	t_fd_list	*fd_list;
+
+	if (fd < 0 || BUFF_SIZE <= 0 || !line)
 		return (-1);
-	if (!fd_str)
-		fd_str = ft_strnew(1);
-	while (!(ft_strchr(fd_str, '\n')) && (ret = 1))
+	fd_list = (fd_list == NULL) ? create_node(fd) : fd_list;
+	while (!ft_strchr(NODE->fd_str, '\n'))
 	{
 		ret = read(fd, read_buf, BUFF_SIZE);
 		if (ret == -1)
 			return (-1);
-		read_buf[ret] = '\0';
-		fd_str = concat_strings(fd_str, read_buf);
-		if (ret == 0 && fd_str[0] == '\0')
-			return (0);
 		if (ret == 0)
-			break ;
+			return (split_str_by_newline(NODE, line));
+		read_buf[ret] = '\0';
+		NODE->tmp = NODE->fd_str;
+		NODE->fd_str = ft_strjoin(NODE->fd_str, read_buf);
+		free(NODE->tmp);
 	}
-	*line = split_by_nl(fd_str, 1);
-	fd_str = split_by_nl(fd_str, 2);
-	return (1);
+	return (split_str_by_newline(NODE, line));
 }
--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -5,13 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pcarolei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/29 06:10:51 by pcarolei          #+#    #+#             */
-/*   Updated: 2019/08/05 02:47:04 by pcarolei         ###   ########.fr       */
+/*   Created: 2019/08/19 11:06:03 by pcarolei          #+#    #+#             */
+/*   Updated: 2019/08/19 11:10:21 by pcarolei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define BUFF_SIZE 32
-#include <stdlib.h>
-#include "./libft/libft.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# include <stdlib.h>
+# include "./libft/libft.h"
 
-int	get_next_line(const int fd, char **line);
+typedef struct	s_fd_list
+{
+	int					fd;
+	char				*fd_str;
+	char				*tmp;
+	struct s_fd_list	*next;
+}				t_fd_list;
+
+int				get_next_line(const int fd, char **line);
+# define NODE (find_node_by_fd(fd, fd_list))
+
+#endif
